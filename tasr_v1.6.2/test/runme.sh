2e771caa1381c238269d4f809b0dcf2184f8deb9
Rene <rwarren@bcgsc.ca>
2019-01-21 15:29:53 -0800

    update data src ftp to http

--- tasr_v1.6.2/test/runme.sh
+++ tasr_v1.6.2/test/runme.sh
@@ -1,7 +1,7 @@
 echo Downloading NGS reads
-wget ftp://ftp.bcgsc.ca/supplementary/SSAKE/SRR066437.fastq.bz2
-#echo Decompressing
-#bunzip2 SRR066437.fastq.bz2
+wget http://www.bcgsc.ca/downloads/supplementary/SSAKE/SRR066437.fastq.bz2
+echo Decompressing
+bunzip2 SRR066437.fastq.bz2
 echo Running TASR in reference-guided assembly mode -i 0
 ../TASR -s targets.fa -f NGSreads.fof -m 15 -c 1 -u 1 -i 0 -b referenceGuided
 echo Running TASR in de novo assembly mode -i 1
